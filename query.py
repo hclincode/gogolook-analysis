03292d7081ce22ae963ad0b808082abc629fb5ef
Lin HongChe <hclin.code@gmail.com>
2020-07-04 11:39:17 +0800

    calculate ratio

--- query.py
+++ query.py
@@ -31,15 +31,47 @@ def task_two(cursor):
 
 QUERY_CONTINUOUS_N_DAYS = (2, 5, 7, 14, 30)
 
+TASK_THREE_STEP_ONE_OUTPUT_PATH = 'data/task_three_step1.csv'
 
-def task_three(cursor):
-    with open('data/task_three.csv', 'w') as fp:
+
+def task_three_step_one(cursor):
+    with open(TASK_THREE_STEP_ONE_OUTPUT_PATH, 'w') as fp:
         writer = csv.writer(fp)
         writer.writerow(('number_of_user', 'continuous_n_days'))
         for n in QUERY_CONTINUOUS_N_DAYS:
-            cursor.execute(SQL_NUM_OF_USER_CONTINUOUS_REPORT_N_DAYS % (n-1))
+            cursor.execute(SQL_NUM_OF_USER_CONTINUOUS_REPORT_N_DAYS % (n - 1))
             for num_of_user in cursor:
                 writer.writerow((num_of_user[0], n))
+
+
+TASK_THREE_STEP_TWO_OUTPUT_PATH = 'data/task_three_step2.csv'
+
+
+def task_three_step_two(cursor):
+    cursor.execute(SQL_NUM_OF_UNIQ_USER)
+    query_result = cursor.fetchone()
+    if not query_result:
+        print '[ERROR] no result from query'
+        return
+
+    total_user_count = query_result[0]
+
+    task_result = []
+    with open(TASK_THREE_STEP_ONE_OUTPUT_PATH, 'r') as data:
+        rows = csv.reader(data)
+        # skip title
+        rows.next()
+
+        for row in rows:
+            number_of_user, continuous_n_days = row
+            ratio_str = '%.4lf' % (float(number_of_user) / total_user_count)
+            task_result.append((ratio_str, str(continuous_n_days)))
+
+    with open(TASK_THREE_STEP_TWO_OUTPUT_PATH, 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(('ratio_of_total_user', 'continuous_n_days'))
+        for ratio_str, continuous_n_days in task_result:
+            writer.writerow((ratio_str, continuous_n_days))
 
 
 if __name__ == '__main__':
@@ -47,6 +79,7 @@ if __name__ == '__main__':
     cursor = cnx.cursor()
 
     # task_two(cursor)
-    #task_three(cursor)
+    # task_three_step_one(cursor)
+    # task_three_step_two(cursor)
 
     cnx.close()
