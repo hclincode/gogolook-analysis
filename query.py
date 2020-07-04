c254355c2042e720b31807a66076b761e2227e8d
Lin HongChe <hclin.code@gmail.com>
2020-07-04 11:16:42 +0800

    finish task 2 3

--- query.py
+++ query.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+import csv
 
 mysql_config = {
     'host': 'localhost',
@@ -8,17 +9,44 @@ mysql_config = {
     'password': os.environ['MYSQL_PASSWORD'],
 }
 
-sql_num_of_uniq_user = '''SELECT COUNT(DISTINCT user_id) FROM report_event'''
+SQL_NUM_OF_UNIQ_USER = '''SELECT COUNT(DISTINCT user_id) FROM report_event'''
 
-sql_num_of_user_on_date = '''SELECT COUNT(DISTINCT user_id) as num_of_user, date(ts) as report_date
+SQL_NUM_OF_USER_ON_DATE = '''SELECT COUNT(DISTINCT user_id) as num_of_user, date(ts) as report_date
 	FROM report_event
     GROUP BY date(ts)'''
 
-sql_num_of_user_who_report_continuous_n_day = '''SELECT count(distinct user_id)
+SQL_NUM_OF_USER_CONTINUOUS_REPORT_N_DAYS = '''SELECT count(distinct user_id) as number_of_user
 FROM continuous_report
 WHERE date_add(start_date, INTERVAL %s day) <= end_date'''
+
+
+def task_two(cursor):
+    with open('data/task_two.csv', 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(('num_of_user', 'report_date'))
+        cursor.execute(SQL_NUM_OF_USER_ON_DATE)
+        for num_of_user, report_date in cursor:
+            writer.writerow((num_of_user, report_date))
+
+
+QUERY_CONTINUOUS_N_DAYS = (2, 5, 7, 14, 30)
+
+
+def task_three(cursor):
+    with open('data/task_three.csv', 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(('number_of_user', 'continuous_n_days'))
+        for n in QUERY_CONTINUOUS_N_DAYS:
+            cursor.execute(SQL_NUM_OF_USER_CONTINUOUS_REPORT_N_DAYS % (n-1))
+            for num_of_user in cursor:
+                writer.writerow((num_of_user[0], n))
+
 
 if __name__ == '__main__':
     cnx = mysql.connector.connect(**mysql_config)
     cursor = cnx.cursor()
+
+    # task_two(cursor)
+    #task_three(cursor)
+
     cnx.close()
